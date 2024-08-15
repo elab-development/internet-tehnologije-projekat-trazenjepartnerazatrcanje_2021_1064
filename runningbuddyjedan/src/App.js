906a34682573984747382467f1497f4f6578f53a
Ivana Nikolic <ivanan@hotmail.rs>
2024-08-15 20:49:53 +0200

    initial.commit

--- runningbuddyjedan/src/App.js
+++ runningbuddyjedan/src/App.js
@@ -1,23 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import { Routes, Route} from "react-router-dom"
+import Navbar from "./components/Navbar";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+import Home from "./pages/Home";
+import Profile from "./pages/Profile"
+import Dashboard from "./pages/Dashbord"
+import ProtectedRoute from "./route/ProtectedRoute";
+import PlanDetails from "./components/PlanDetails";
+import AdminDashboard from "./pages/AdminDashboard";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+
+      <Navbar />
+        <Routes>
+          {/* home */}
+          <Route path='/' element= {<Home />} />
+          {/* register */}
+          <Route path='/register' element= {<Register />} />
+          {/* login */}
+          <Route path='/login' element= {<Login />} />
+          {/* dashboard */}
+          <Route
+            path="/dashboard"
+            element={
+              <ProtectedRoute>
+                <Dashboard />
+              </ProtectedRoute>
+            }
+          />
+          {/* adminDashboard */}
+          <Route
+            path="/admin"
+            element={
+              <ProtectedRoute>
+                <AdminDashboard />
+              </ProtectedRoute>
+            }
+          />
+          {/* detaljiplanova */}
+          <Route path="/plan/:id" element={<ProtectedRoute><PlanDetails /></ProtectedRoute>} />
+          {/* profil */}
+          <Route
+            path="/profile"
+            element={
+              <ProtectedRoute>
+                <Profile />
+              </ProtectedRoute>
+            }
+          />
+
+        </Routes>
+        
     </div>
   );
 }
