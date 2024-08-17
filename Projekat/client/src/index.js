ae853b2cd544c7e8898f67dc8539a34b09116622
Danijela Pudaric <danijelapudaric62@gmail.com>
2024-08-18 01:00:02 +0200

    Pages

--- Projekat/client/src/index.js
+++ Projekat/client/src/index.js
@@ -2,16 +2,35 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { BrowserRouter } from 'react-router-dom';
+import { AuthProvider } from "./context/AuthContext";
+import { PlansContextProvider } from './context/PlansContext';
+import { ItineraryContextProvider } from './context/ItineraryContext';
+import { CommentContextProvider } from './context/CommentContext';
+import 'leaflet/dist/leaflet.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    {/* da mozemo da se navodimo po stranicama */}
+    <BrowserRouter>
+    {/* auth provider */}
+    <AuthProvider>
+      {/* plans provider */}
+      <PlansContextProvider>
+        {/* itinerary provider */}
+        <ItineraryContextProvider>
+          {/* comment provider */}
+          <CommentContextProvider>
+            <App />
+          </CommentContextProvider>
+      
+      </ItineraryContextProvider>
+      </PlansContextProvider>
+      </AuthProvider>
+</BrowserRouter>
   </React.StrictMode>
+  
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
