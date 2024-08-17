d58c45965ffa45681f34308306d4cba168df1287
Ivana Nikolic <ivanan@hotmail.rs>
2024-08-17 23:56:56 +0200

    Pages

--- Projekat/client/src/App.js
+++ Projekat/client/src/App.js
@@ -1,15 +1,61 @@
-import React from 'react';
-import './App.css';
+import { Routes, Route} from "react-router-dom"
+import Navbar from "./components/Navbar";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+import Home from "./pages/Home";
+import Profile from "./pages/Profile"
+import Dashboard from "./pages/Dashbord"
+import ProtectedRoute from "./route/ProtectedRoute";
+import PlanDetails from "./components/PlanDetails";
+import AdminDashboard from "./pages/AdminDashboard";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Welcome to My React App</h1>
-      </header>
+      
+
+      <Navbar />
+        <Routes>
+          {/* home */}
+          <Route path='/' element= {<Home />} />
+          {/* register */}
+          <Route path='/register' element= {<Register />} />
+          {/* login */}
+          <Route path='/login' element= {<Login />} />
+          {/* dashboard */}
+          <Route
+            path="/dashboard"
+            element={
+              <ProtectedRoute>
+                <Dashboard />
+              </ProtectedRoute>
+            }
+          />
+          {/* adminDashboard */}
+          <Route
+            path="/admin"
+            element={
+              <ProtectedRoute>
+                <AdminDashboard />
+              </ProtectedRoute>
+            }
+          />
+          {/* detaljiplanova */}
+          <Route path="/plan/:id" element={<ProtectedRoute><PlanDetails /></ProtectedRoute>} />
+          {/* profil */}
+          <Route
+            path="/profile"
+            element={
+              <ProtectedRoute>
+                <Profile />
+              </ProtectedRoute>
+            }
+          />
+
+        </Routes>
+        
     </div>
   );
 }
 
 export default App;
-
